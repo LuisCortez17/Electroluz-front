9f65e822450925034680e6771b6f175c9c50738c
ser1nlu <sergio.inga@urp.edu.pe>
2024-07-25 00:23:49 -0500

    m

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import { FaBars, FaHome, FaShoppingCart, FaTimes, FaUser } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
 
   return (
     <nav className="bg-blue-800 text-white p-4 flex items-center justify-between">
       <div className="flex items-center space-x-4">
-        <button className="focus:outline-none">
+        <Link className="focus:outline-none" to="/">
           <h1 className='pl-2 text-3xl font-bold text-white-900 tracking-tight leading-tight'>
             ELECTROLUZ
             </h1>
-        </button>
+        </Link>
         <button className="focus:outline-none">
           <FaBars className="text-white" />
         </button>
@@ -26,9 +27,9 @@ const Navbar: React.FC = () => {
         <button className="focus:outline-none">
           <FaShoppingCart className="text-white" />
         </button>
-        <button className="focus:outline-none">
+        <Link to="/login" className="focus:outline-none">
           <FaUser className="text-white" />
-        </button>
+        </Link>
       </div>
     </nav>
   );
